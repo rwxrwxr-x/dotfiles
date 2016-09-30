881c3862ceb30d8e791fd5ab55efb6ca7dfbb54f
quanttyo <divergere.ratio@Gmail.com>
2016-09-30 04:38:33 +0300

    i3 upd

--- .config/i3blocks/scripts/battery.py
+++ .config/i3blocks/scripts/battery.py
@@ -7,37 +7,43 @@ RED      = "%{F#FF0000}"
 
 
 # BATTERIES
-B_CRITICAL = ""   #   5%
-B_LOW      = ""   #  25%
-B_HALF     = ""   #  50%
-B_ALMOST   = ""   #  75%
-B_FULL     = ""   # 100%
+BATTERY_CRITICAL = " "   #   5%
+BATTERY_LOW      = ""  #  25%
+BATTERT_HALF     = ""   #  50%
+BATTERY_ALMOST   = ""   #  75%
+BATTERY_FULL     = " "   # 100%
 WIRED      = ""
 
+#def run(arg):
+ # return subprocess.Popen(arg, stdout=subprocess.PIPE, stderr=subprocess.PIPE).stdout.read().decode(encoding="UTF-8")
 def run(arg):
-  return subprocess.Popen(arg, stdout=subprocess.PIPE, stderr=subprocess.PIPE).stdout.read().decode(encoding="UTF-8")
+   # from subprocess import Popen,PIPE
+    return subprocess.Popen(arg,stdout = subprocess.PIPE,stderr = subprocess.PIPE,shell = True).communicate()[0].decode("utf-8").strip()
 
 
-def battery():
-  raw     = run(["acpi"])[11:-1]
-  icon    = ""
-  time    = ""
-  time = raw[raw.find("%,")+4:raw.find("%,")+8]
-  if "unknown" in raw:
-    icon = WIRED + " "
-    if time == "unti": time = ""
-  elif "Full" in raw:
-    icon = WIRED
-    time = ""
-  elif "remaining" in raw:
-    percent = int(raw[raw.find(" ")+1:raw.find("%")])
-    if   percent <= 5:   icon = B_CRITICAL
-    elif percent <= 25:  icon = B_LOW
-    elif percent <= 50:  icon = B_HALF
-    elif percent <= 75:  icon = B_ALMOST
-    elif percent <= 100: icon = B_FULL
-    icon += " "
-    if icon == B_LOW + " ": return RED + icon + time
-  return icon + time
 
-print(battery())
+
+def get_battery():
+    icon    = ""; time    = ""
+    raw     = run("acpi")[11:-1]
+    time = raw[raw.find("%,")+4:raw.find("%,")+8]
+    if "until" in raw:
+      icon = WIRED + " "
+      if time == "unti": time = ""
+    elif "Unknown" in raw:
+      icon = WIRED
+      time = ""
+    elif "Full" in raw:
+      icon =  WIRED
+      time = ""
+    elif "remainin" in raw:
+      percent = int(raw[raw.find(" ")+1:raw.find("%")])
+      if   percent <= 5:   icon = BATTERY_CRITICAL
+      elif percent <= 25:  icon = BATTERY_LOW
+      elif percent <= 50:  icon = BATTERY_HALF
+      elif percent <= 75:  icon = BATTERY_ALMOST
+      elif percent <= 100: icon = BATTERY_FULL
+      icon += " "
+      if icon == BATTERY_LOW + " ": return icon + time
+    return icon + str(time)
+print(get_battery())
