d515e6ba565432c9ed9b0c5ba7a165f1083be72a
quanttyo <divergere.ratio@Gmail.com>
2016-10-08 06:01:43 +0300

    clean

--- .config/x11/i3.sh
+++
@@ -1,9 +0,0 @@
-# start i3 window manager 
-# source this script from xinitrc
-
-# compositor and wallpaper
-compton --config ~/.config/compton/compton.conf -b --backend glx --vsync opengl-swc &
-#feh --randomize --bg-fill ~/.bg/* &
-feh --bg-fill ~/.bg/eva.jpeg &
-
-exec i3
