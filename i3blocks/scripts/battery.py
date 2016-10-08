adff802f4375501bae42aaa98036f40e631f65f7
quanttyo <divergere.ratio@Gmail.com>
2016-10-08 06:04:30 +0300

    upd

---
+++ i3blocks/scripts/battery.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+import time, subprocess, os
+
+# BATTERIES
+BATTERY_CRITICAL = " "   #   5%
+BATTERY_LOW      = ""  #  25%
+BATTERT_HALF     = ""   #  50%
+BATTERY_ALMOST   = ""   #  75%
+BATTERY_FULL     = " "   # 100%
+WIRED      = ""
+
+formatinit = "<span background=\"#38253d\""
+formatcolor = " foreground=\"#c98acf\">"
+formatend = " </span>"
+#def run(arg):
+ # return subprocess.Popen(arg, stdout=subprocess.PIPE, stderr=subprocess.PIPE).stdout.read().decode(encoding="UTF-8")
+def run(arg):
+   # from subprocess import Popen,PIPE
+    return subprocess.Popen(arg,stdout = subprocess.PIPE,stderr = subprocess.PIPE,shell = True).communicate()[0].decode("utf-8").strip()
+
+
+
+
+def get_battery():
+    icon    = ""; time    = ""
+    raw     = run("acpi")[11:-1]
+    time = raw[raw.find("%,")+4:raw.find("%,")+8]
+    if "until" in raw:
+        icon = WIRED + ""
+        if time == "unti": time = ""
+    elif "Unknown" in raw:
+        icon = WIRED
+        time = ""
+    elif "Full" in raw:
+        icon =  WIRED
+        time = ""
+    elif "remainin" in raw:
+        percent = int(raw[raw.find(" ")+1:raw.find("%")])
+        if   percent <= 5:   icon = BATTERY_CRITICAL; formatcolor = "foreground=\"#ff0000\">"
+        elif percent <= 25:  icon = BATTERY_LOW
+        elif percent <= 50:  icon = BATTERY_HALF
+        elif percent <= 75:  icon = BATTERY_ALMOST
+        elif percent <= 100: icon = BATTERY_FULL
+        icon += " "
+        if icon == BATTERY_LOW + " ": return icon + time
+    return icon + str(time)
+print(str(formatinit + formatcolor + get_battery() + formatend))
---
+++ i3blocks/scripts/volume.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+import time, subprocess, os
+
+# Search your device in "pactl list sinks" output, and change SoundDev on "name" of device
+# For example:
+# Audiodevice №3
+#    Состояние: IDLE
+#    Имя: alsa_output.usb-Logitech_Logitech_USB_Headset-00.analog-stereo
+#    Description: Logitech USB Headset
+#    Driver: module-alsa-card.c
+
+SoundDev =  "alsa_output.usb-Logitech_Logitech_USB_Headset-00.analog-stereo"
+
+
+def run(arg):
+    return subprocess.Popen(arg, stdout=subprocess.PIPE, stderr=subprocess.PIPE).stdout.read().decode(encoding="UTF-8")
+
+def searchMethod():
+  raw = run(["pactl", "list", "sinks"]).splitlines()[8]
+  return raw
+
+def device(dev,find):
+  raw   = run(["pactl","list","sinks"]).splitlines()
+  y = 0
+  x = 0
+  n = 0
+  for x in range (0,150):
+      if dev in raw[x]:
+        y = x
+        for y in range (x,150):
+          if find in raw[y]:
+            n = y
+            break
+        break
+  return n+1
+
+def volume(num):
+  raw   = run(["pactl", "list", "sinks"]).splitlines()[num:num+1][0]
+  value = raw[raw.find("/ ")+2:raw.find("%")]
+  return value
+
+print(volume(device(SoundDev,searchMethod())))
