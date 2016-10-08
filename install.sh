8ee74b6537f05e8f6fa94533949b025b28015ee3
quanttyo <divergere.ratio@Gmail.com>
2016-10-08 06:08:09 +0300

    install script

---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+configpath=$HOME/test/.config/
+
+echo "Copy config files"
+sleep 2
+mkdir -p $configpath
+cp -i -R compton/ $configpath
+cp -i -R htop/ $configpath
+cp -i -R i3/ $configpath
+cp -i -R i3blocks/ $configpath
+cp -i -R neofetch/ $configpath
+cp -i -R qt5ct/ $configpath
+cp -i -R ranger/ $configpath
+cp -i -R scripts/ $configpath
+cp -i -R x11/ $configpath
+cp -i -R ncmpcpp/ $homee/.ncmpcpp/
+cp -i -R bg/ $homee/.bg/
+cp -i -R .tmux.conf $HOME
+cp -i -R .Xdefaults $HOME
+cp -i -R .xinitrc $HOME
+cp -i -R .zshrc $HOME
+cp -i -R fonts/ $HOME/.fonts/
+echo "end"
+sleep 1
+cd $homee/.fonts
+echo $(pwd)
+fc-cache -vf
+
---
+++ x11/i3.sh
@@ -0,0 +1,9 @@
+# start i3 window manager 
+# source this script from xinitrc
+
+# compositor and wallpaper
+compton --config ~/.config/compton/compton.conf -b --backend glx --vsync opengl-swc &
+#feh --randomize --bg-fill ~/.bg/* &
+feh --bg-fill ~/.bg/eva.jpeg &
+
+exec i3
