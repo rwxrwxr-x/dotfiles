76a9fefa489267bd82eae72394a1e2d6a2ddafc8
quanttyo <divergere.ratio@gmail.com>
2021-04-29 11:39:48 +0300

    Clean misc deleted

--- scripts/searchbuff.sh
+++
@@ -1,1 +0,0 @@
-xclip -o | sed -r '2~1d;s/(^\s+|\s+$)//g;s/%/%25/g;s/#/%23/g;s/\$/%24/g;s/&/%26/g;s/\+/%2B/;s/,/%2C/g;s/:/%3A/g;s/;/%3B/g;s/=/%3D/g;s/\?/%3F/g;s/@/%40/g;s/\s/+/g' | awk '{print "http://www.google.ru/search?hl=ru&q=" $1}' | xargs firefox-bin -new-tab
