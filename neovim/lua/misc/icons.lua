c3bae77dbfe4c482df1a8c36f7b5a97ac903f5fa
quanttyo <divergere.ratio@gmail.com>
2021-07-10 20:10:00 +0300

    Misc

---
+++ neovim/lua/misc/icons.lua
@@ -0,0 +1,31 @@
+local icons = {
+    statusline = {
+    border = {
+        right = "", -- U-E0B6
+        left = "" -- U-E0BE
+    },
+    diff = {
+        Add = "",
+        Modified = "",
+        Remove = ""
+    },
+    git = "   ",
+    mode = {
+        c = "🅒",
+        i = "🅘",
+        n = "🅝",
+        r = "🅡",
+        s = "🅢",
+        t = "🅣",
+        v = "🅥"
+    },
+    diagnostic = {
+        error = " ",
+        warn = " ",
+        info = " "
+    },
+    bullet = "•"
+}
+}
+
+return icons
