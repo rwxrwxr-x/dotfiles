243422e465530f085ab46b75914e0b04be15142c
rawpull <divergere.ratio@gmail.com>
2021-07-14 20:10:00 +0300

    competion-core

---
+++ neovim/lua/core/compe.lua
@@ -0,0 +1,92 @@
+local M, g, fn, api = {}, vim.g, vim.fn, vim.api
+
+M.config = function()
+  O.completion = {
+    enabled = true,
+    autocomplete = true,
+    debug = false,
+    min_length = 1,
+    preselect = "enable",
+    throttle_time = 80,
+    source_timeout = 200,
+    incomplete_delay = 400,
+    max_abbr_width = 100,
+    max_kind_width = 100,
+    max_menu_width = 100,
+    documentation = true,
+
+    source = {
+      path = { kind = "   (Path)" },
+      buffer = { kind = "   (Buffer)" },
+      calc = { kind = "   (Calc)" },
+      vsnip = { kind = "   (Snippet)" },
+      nvim_lsp = { kind = "   (LSP)" },
+      nvim_lua = false,
+      spell = { kind = "   (Spell)" },
+      tags = false,
+      vim_dadbod_completion = false,
+      snippets_nvim = false,
+      ultisnips = false,
+      treesitter = false,
+    },
+  }
+end
+
+M.setup = function()
+  g.vsnip_snippet_dir = O.vsnip_dir
+
+  local status_ok, compe = pcall(require, "compe")
+  if not status_ok then
+    return
+  end
+
+  compe.setup(O.completion)
+
+  local t = function(str)
+    return api.nvim_replace_termcodes(str, true, true, true)
+  end
+
+  local check_back_space = function()
+    local col = fn.col "." - 1
+    if col == 0 or fn.getline("."):sub(col, col):match "%s" then
+      return true
+    else
+      return false
+    end
+  end
+
+  _G.tab_complete = function()
+    if fn.pumvisible() == 1 then
+      return t "<C-n>"
+    elseif fn.call("vsnip#available", { 1 }) == 1 then
+      return t "<Plug>(vsnip-expand-or-jump)"
+    elseif check_back_space() then
+      return t "<Tab>"
+    else
+      return fn["compe#complete"]()
+    end
+  end
+
+  _G.s_tab_complete = function()
+    if fn.pumvisible() == 1 then
+      return t "<C-p>"
+    elseif fn.call("vsnip#jumpable", { -1 }) == 1 then
+      return t "<Plug>(vsnip-jump-prev)"
+    else
+      return t "<S-Tab>"
+    end
+  end
+
+  api.nvim_set_keymap("i", "<Tab>", "v:lua.tab_complete()", { expr = true })
+  api.nvim_set_keymap("s", "<Tab>", "v:lua.tab_complete()", { expr = true })
+  api.nvim_set_keymap("i", "<S-Tab>", "v:lua.s_tab_complete()", { expr = true })
+  api.nvim_set_keymap("s", "<S-Tab>", "v:lua.s_tab_complete()", { expr = true })
+
+  api.nvim_set_keymap("i", "<C-Space>", "compe#complete()", { noremap = true, silent = true, expr = true })
+  api.nvim_set_keymap("i", "<CR>", "compe#confirm('<CR>')", { noremap = true, silent = true, expr = true })
+  api.nvim_set_keymap("i", "<C-e>", "compe#close('<C-e>')", { noremap = true, silent = true, expr = true })
+  api.nvim_set_keymap("i", "<C-f>", "compe#scroll({ 'delta': +4 })", { noremap = true, silent = true, expr = true })
+  api.nvim_set_keymap("i", "<C-d>", "compe#scroll({ 'delta': -4 })", { noremap = true, silent = true, expr = true })
+end
+
+return M
