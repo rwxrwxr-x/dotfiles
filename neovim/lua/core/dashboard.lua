c5ac024eccca2ed6df25d71666cd2dc68eb374bc
rawpull <divergere.ratio@gmail.com>
2021-07-18 21:55:00 +0300

    dashboard

--- neovim/lua/core/dashboard.lua
+++ neovim/lua/core/dashboard.lua
@@ -1,32 +1,82 @@
 local M, g = {}, vim.g
 
 M.config = function()
-    O.plugin.dashboard = {
-        active = false,
-        custom_header = {
-            "{{ header }}"
-        },
-    }
+  O.plugin.dashboard = {
+    active = false,
+    search_handler = "telescope",
+    custom_header = {
+      "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
+      "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
+      "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
+      "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣶⣶⣶⣶⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
+      "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠛⠛⢻⣿⣿⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
+      "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣿⣿⣷⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
+      "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣾⣿⣿⣿⣇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
+      "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⣿⡟⢹⣿⣿⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
+      "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⣿⣿⠏⠀⠀⢻⣿⣿⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
+      "⠀⠀⠀⠀⠀⠀⠀⠀⠀⣴⣿⡿⠃⠀⠀⠀⠈⢿⣿⣷⣤⣤⡆⠀⠀⠀⠀⠀⠀⠀",
+      "⠀⠀⠀⠀⠀⠀⠀⠀⠾⠿⠿⠁⠀⠀⠀⠀⠀⠘⣿⣿⡿⠿⠛⠀⠀⠀⠀⠀⠀⠀",
+      "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
+      "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
+      "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
+      "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
+    },
+
+    custom_section = {
+      a = {
+        description = { "  Find File          " },
+        command = "Telescope find_files",
+      },
+      b = {
+        description = { "  Recently Used Files" },
+        command = "Telescope oldfiles",
+      },
+      c = {
+        description = { "  Find Word          " },
+        command = "Telescope live_grep",
+      },
+      d = {
+        description = { "  Settings           " },
+        command = ":e " .. CONFIG_PATH .. "/lv-config.lua",
+      },
+    },
+
+    footer = { "chrisatmachine.com" },
+  }
 end
 
 M.setup = function()
-    g.dashboard_disable_at_vimenter = 0
+  g.dashboard_disable_at_vimenter = 0
 
-    g.dashboard_custom_header = O.plugin.dashboard.custom_header
+  g.dashboard_custom_header = O.plugin.dashboard.custom_header
 
-    g.dashboard_default_executive = O.plugin.dashboard.search_handler
+  g.dashboard_default_executive = O.plugin.dashboard.search_handler
 
-    g.dashboard_custom_section = O.plugin.dashboard.custom_section
+  vim.g.dashboard_custom_section = O.plugin.dashboard.custom_section
+  vim.cmd "let g:dashboard_session_directory = $HOME..'/.config/nvim/.sessions'"
+  vim.cmd "let packages = len(globpath('~/.local/share/nvim/site/pack/packer/start', '*', 0, 1))"
 
-    vim.cmd "let g:dashboard_session_directory = $HOME..'/.config/nvim/.sessions'"
-    vim.cmd "let packages = len(globpath('~/.local/share/nvim/site/pack/packer/start', '*', 0, 1))"
-  
-    vim.api.nvim_exec(
-      [[
-      let g:dashboard_custom_footer = ['LuaJIT loaded '..packages..' plugins']
-  ]],
-      false
-    )
+  vim.api.nvim_exec(
+    [[
+    let g:dashboard_custom_footer = ['LuaJIT loaded '..packages..' plugins']
+]],
+    false
+  )
+  require("lv-utils").define_augroups {
+    _dashboard = {
+      {
+        "FileType",
+        "dashboard",
+        "setlocal nocursorline noswapfile synmaxcol& signcolumn=no norelativenumber nocursorcolumn nospell  nolist  nonumber bufhidden=wipe colorcolumn= foldcolumn=0 matchpairs= ",
+      },
+      {
+        "FileType",
+        "dashboard",
+        "set showtabline=0 | autocmd BufLeave <buffer> set showtabline=2",
+      },
+      { "FileType", "dashboard", "nnoremap <silent> <buffer> q :q<CR>" },
+    },
+  }
 end
 
 return M
